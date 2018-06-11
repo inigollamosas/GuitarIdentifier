d86b56149e3b67e773e3737280ef3e0b404da41a
Inigo <inigollamosas@gmail.com>
2018-06-11 08:57:00 +0200

    delete unnecessary code

--- GuitarIdentifier/Controllers/PhotoViewController.swift
+++ GuitarIdentifier/Controllers/PhotoViewController.swift
@@ -13,39 +13,16 @@ class PhotoViewController: UIViewController  {
                                      attributes: [],
                                      target: nil)
     
-    var previewLayer : AVCaptureVideoPreviewLayer!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     var videoDeviceInput: AVCaptureDeviceInput!
-    var setupResult: SessionSetupResult = .success
-    
-    enum SessionSetupResult {
-        case success
-        case notAuthorized
-        case configurationFailed
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        checkAuthorization()
-        
-        /*
-         Setup the capture session.
-         In general it is not safe to mutate an AVCaptureSession or any of its
-         inputs, outputs, or connections from multiple threads at the same time.
-         
-         Why not do all of this on the main queue?
-         Because AVCaptureSession.startRunning() is a blocking call which can
-         take a long time. We dispatch session setup to the sessionQueue so
-         that the main queue isn't blocked, which keeps the UI responsive.
-         */
         sessionQueue.async { [unowned self] in
             self.configureSession()
         }
-        
         photoButton.layer.cornerRadius = 10
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: #selector(getPhotoFromLibraryPressed))
-        
-
     }
     
     @objc func getPhotoFromLibraryPressed() {
@@ -56,105 +33,29 @@ class PhotoViewController: UIViewController  {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
+        super.viewWillAppear(animated)
         sessionQueue.async {
-            switch self.setupResult {
-            case .success:
-                // Only start the session running if setup succeeded.
-                DispatchQueue.main.async { [unowned self] in
-                    self.previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
-                    self.previewLayer.videoGravity = .resizeAspectFill
-                    self.previewLayer.frame = self.previewView.bounds
-                    self.previewView.layer.addSublayer(self.previewLayer)
-                    self.session.startRunning()
-                }
-                
-            case .notAuthorized:
-                DispatchQueue.main.async { [unowned self] in
-                    let changePrivacySetting = "AVCam doesn't have permission to use the camera, please change privacy settings"
-                    let message = NSLocalizedString(changePrivacySetting, comment: "Alert message when the user has denied access to the camera")
-                    let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
-                    
-                    alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"),
-                                                            style: .cancel,
-                                                            handler: nil))
-                    
-                    alertController.addAction(UIAlertAction(title: NSLocalizedString("Settings", comment: "Alert button to open Settings"),
-                                                            style: .`default`,
-                                                            handler: { _ in
-                                                                UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!, options: [:], completionHandler: nil)
-                    }))
-                    
-                    self.present(alertController, animated: true, completion: nil)
-                }
-                
-            case .configurationFailed:
-                DispatchQueue.main.async { [unowned self] in
-                    let alertMsg = "Alert message when something goes wrong during capture session configuration"
-                    let message = NSLocalizedString("Unable to capture media", comment: alertMsg)
-                    let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
-                    
-                    alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"),
-                                                            style: .cancel,
-                                                            handler: nil))
-                    
-                    self.present(alertController, animated: true, completion: nil)
-                }
+            DispatchQueue.main.async { [unowned self] in
+                self.previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                self.previewLayer.videoGravity = .resizeAspectFill
+                self.previewLayer.frame = self.previewView.bounds
+                self.previewView.layer.addSublayer(self.previewLayer)
+                self.session.startRunning()
             }
         }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         sessionQueue.async { [unowned self] in
-            if self.setupResult == .success {
-                self.session.stopRunning()
-            }
+            self.session.stopRunning()
         }
-        
         super.viewWillDisappear(animated)
     }
     
     // MARK: Session Management
     
-    func checkAuthorization() {
-        /*
-         Check video authorization status. Video access is required and audio
-         access is optional. If audio access is denied, audio is not recorded
-         during movie recording.
-         */
-        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
-        case .authorized:
-            // The user has previously granted access to the camera.
-            break
-            
-        case .notDetermined:
-            /*
-             The user has not yet been presented with the option to grant
-             video access. We suspend the session queue to delay session
-             setup until the access request has completed.
-             
-             Note that audio access will be implicitly requested when we
-             create an AVCaptureDeviceInput for audio during session setup.
-             */
-            sessionQueue.suspend()
-            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { [unowned self] granted in
-                if !granted {
-                    self.setupResult = .notAuthorized
-                }
-                self.sessionQueue.resume()
-            })
-            
-        default:
-            // The user has previously denied access.
-            setupResult = .notAuthorized
-        }
-    }
-    
     private func configureSession() {
-        if setupResult != .success {
-            return
-        }
         
         session.beginConfiguration()
         session.sessionPreset = AVCaptureSession.Preset.photo
@@ -190,15 +91,9 @@ class PhotoViewController: UIViewController  {
                 session.addInput(videoDeviceInput)
                 self.videoDeviceInput = videoDeviceInput
                 
-            } else {
-                print("Could not add video device input to the session")
-                setupResult = .configurationFailed
-                session.commitConfiguration()
-                return
             }
         } catch {
             print("Could not create video device input: \(error)")
-            setupResult = .configurationFailed
             session.commitConfiguration()
             return
         }
@@ -206,12 +101,10 @@ class PhotoViewController: UIViewController  {
         // Add photo output.
         if session.canAddOutput(photoOutput) {
             session.addOutput(photoOutput)
-            
             photoOutput.isHighResolutionCaptureEnabled = true
             photoOutput.isLivePhotoCaptureEnabled = photoOutput.isLivePhotoCaptureSupported
         } else {
             print("Could not add photo output to the session")
-            setupResult = .configurationFailed
             session.commitConfiguration()
             return
         }
@@ -245,8 +138,8 @@ class PhotoViewController: UIViewController  {
 }
 
 extension PhotoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
     //    MARK: - Delegate methods
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         picker.dismiss(animated: true)
         capturedImage = (info[UIImagePickerControllerOriginalImage] as! UIImage)
@@ -258,19 +151,18 @@ extension PhotoViewController: UIImagePickerControllerDelegate, UINavigationCont
 
 extension PhotoViewController: AVCapturePhotoCaptureDelegate {
     
-//    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
-//
-//        if let error = error {
-//            print("Error capturing photo: \(error)")
-//        } else {
-//            if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
-//
-//                if let image = UIImage(data: dataImage) {
-//                    self.capturedImage.image = image
-//                }
-//            }
-//        }
-//    }
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+
+        if let error = error {
+            print("Error capturing photo: \(error)")
+        } else {
+            if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+                if let image = UIImage(data: dataImage) {
+                    capturedImage = image
+                }
+            }
+        }
+    }
     
     @available(iOS 11.0, *)
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
